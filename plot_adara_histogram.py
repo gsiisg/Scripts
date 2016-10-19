499eb1a36cbb31c3d4043422380f38c143ba005b
gsiisg <gsiisg@gmail.com>
2016-10-18 21:22:14 -0700

    plot_adara_histogram.py

---
+++ plot_adara_histogram.py
@@ -0,0 +1,38 @@
+# Geoffrey So 9/30/2016
+
+import numpy as np
+import pandas as pd
+from collections import Counter
+from matplotlib import pyplot as plt
+
+# data mining test 1
+
+df1=pd.read_csv('data_mining_test_1.csv')
+
+print('any data null ', df.isnull().values.any())
+
+# plot histogram
+
+counts = Counter(df1.columns.values)
+plot_df = pd.DataFrame.from_dict(counts, orient='index')
+plot_df.plot(kind='bar')
+
+for column_name in df1.columns.values:
+    plt.figure();
+    df1[column_name].plot.hist(title=column_name)
+
+# data mining test 2
+
+df2=pd.read_csv('data_mining_test_2.csv')
+
+print('any data null ', df2.isnull().values.any())
+
+# plot histogram
+
+counts = Counter(df2.columns.values)
+plot_df = pd.DataFrame.from_dict(counts, orient='index')
+plot_df.plot(kind='bar')
+
+for column_name in df2.columns.values[1:]:
+    plt.figure();
+    df2[column_name].plot.hist(title=column_name)
